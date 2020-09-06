567da67cbb68567d87afb5835dcab04662d10ade
Muhammad Salman <30803314+msalman899@users.noreply.github.com>
2020-09-06 20:32:50 +0500

    Update cloudformation.py

--- Automation/Cloudformation/cloudformation.py
+++ Automation/Cloudformation/cloudformation.py
@@ -3,21 +3,36 @@ import os
 import sys
 import boto3
 
-Class Cloudformation:
+class Cloudformation:
 
-  def __init__(self,region,profile=None):
-    self.region = region
-    self.profile = profile
-    self.cf_client_session = self.get_session(self.region,self.profile)
-    self.cf_client = cf_client_session.client('cloudformation')
+    def __init__(self, region=None, profile=None):
+        self.region = region
+        self.profile = profile
+        self.cf_client_session = self.get_session(self.region, self.profile)
+        self.cf_client = self.cf_client_session.client('cloudformation')
+
+
+    def get_session(self):
+        if not self.region:
+          print("Unable to create boto3 session, please define region")
+          return
+        elif self.profile:
+            return boto3.session(profile=self.profile, region=self.region)
+        else:
+            return boto3.session(region=self.region)
+                
+    def create_stack(self):
+        pass
     
-  def get_session(self,self.region,self.profile):
-    if self.profile:
-      return boto3.session(profile=self.profile,region=self.region)
-    else:
-      return boto3.session(region=self.region)
+    def delete_stack(self):
+        pass
     
-  def search_stacks_by_name(self,*args):
-    for string in *args
-    if all(x in a_string for x in matches):
+    def update_stack(self):
+        pass
     
+    def wait_stack(self):
+        pass
+    
+    def search_stacks_by_name(self, *args):
+        for string in *args
+            if all(x in a_string for x in matches):
