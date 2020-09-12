79aa517af3b515ee4a5ad853142aea16aeddef5f
Muhammad Salman <30803314+msalman899@users.noreply.github.com>
2020-09-13 01:55:01 +0500

    Update scaleEnvironment.py

--- Automation/Elasticbeanstalk/scaleEnvironment.py
+++ Automation/Elasticbeanstalk/scaleEnvironment.py
@@ -3,55 +3,49 @@ import os
 import sys
 import boto3
 from botocore.exceptions import ClientError
-import elasticbeanstalk
+import json
 from common.boto3Session import *
-
-
-def checkTraffic(r53_client):
-    r53_resource_records = r53_client.list_resource_record_sets(HostedZoneId='string')
-    
-    for record in r53_resource_records:
-        if record['Name'] == recordset_name and record['SetIdentifier'] == commit_id:
-            if record['weight'] == 100:
-                return True
-            return False
-            break
-        return "Not Exist"
-    
+from route53.route53 import *
 
 def main():
     # set up boto3 session
-    custom_session = Customsession(region="eu-west-1",profile="name",)
+    custom_session = Customsession(region="eu-west-1",profile="cpay_sandbox",)
 
     # elasticbeanstalk client object
-    eb_environment = custom_session.client('elasticbeanstalk')
+    eb_client = custom_session.client('elasticbeanstalk')
     # route53 client object
-    r53_client = custom_session.client('route53')
-
-    # initiialize another object that has more advance functions, just pass in the boto3 client object
-    #eb = Elasticbeanstalk(eb_environment)
-    
+    r53_client = custom_session.session.client('route53')
+    #
+    #initiialize another object that has more advance functions, just pass in the boto3 client object
+    r53 = Route53(r53_client)
+    # # eb = Elasticbeanstalk(eb_environment)
+    #
     global commit_id
     global recordset_name
-    
-    application_name=""
-    environment_name=""
-    instance=""
-    environment=""
-    commit_id=""
-    recordset_name=environment+"."+application_name+"."+commit_id
 
-    #check traffic
-    traffic = checkTraffic(r53_client)
-    if traffic:
+    application_name = ""
+    environment_name = ""
+    instance = ""
+    environment = ""
+    commit_id = ""
+    recordset_name = ""
+
+    recordset = r53.search_recordset(HostedZoneId="",Type="",SetIdentifier="")
+    #print(recordset)
+
+    if len(recordset) != 1:
+        print("either multiple record set found OR No record set exist")
+        sys.exit(1)
+
+    if recordset['Weight'] >= 100:
         print("This Env is serving 100% Traffic Scale In/Out is not allowed")
-        sys.exit(1)    
-    elif traffic == "Not Exist":
-        print("Route53 RecordSet Not Found")
+        sys.exit(1)
+
+    print("Traffic is less than 100%, Proceeding for Scale In/Out")
 
     # updating beanstalk environment
     try:
-        eb_update_response = eb_environment.update_environment(
+        eb_update_response = eb_client.update_environment(
             ApplicationName=application_name,
             EnvironmentName=environment_name,
             OptionSettings=[
@@ -68,12 +62,20 @@ def main():
         sys.exit(1)
 
     print("Waiting for Environment Update to be Completed")
-    environment_update_status = eb_environment.get_waiter('environment_updated')
-
-    if not environment_update_status:
-        print("Failed to update environment ",environment_name)
+    try:
+        waiter = eb_client.get_waiter('environment_updated')
+        waiter.wait(
+            ApplicationName='string',
+            EnvironmentNames=[environment_name],
+            WaiterConfig={
+                'Delay': 10,
+                'MaxAttempts': 20
+            }
+        )
+        print("Environment ", environment_name, " has been updated successfully")
+    except Exception as err:
+        print("Failed to update environment ",environment_name + "\n" + str(err))
         sys.exit(1)
-    print("Environment ", environment_name, " has been updated successfully")
 
 
 if __name__ == "__main__":
